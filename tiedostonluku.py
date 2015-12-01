66f48e5ab9f8038c403bb87f28d58571851d083c
aajarven <anni.jarvenpaa@gmail.com>
2015-12-01 16:43:30 +0200

    omat luku- ja kirjoitusfunktiot

--- tiedostonluku.py
+++ tiedostonluku.py
@@ -1,12 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Runge-Kutta -algoritmin toteuttamiseen tarvittavat tiedostonlukufunktiot
-
-@author: Anni Järvenpää
-"""
-
-"""
-Lukee tiedostosta matriisit X0, V0 ja M.
 Matriisin alkiot erotetaan toisistaan pilkulla, matriisit puolipisteellä. Rivejä, jotka alkavat merkillä # ei huomioida.
 Esimerkiksi seuraava tiedosto:
 #X0	        V0              M
@@ -16,6 +10,13 @@ tuottaa matriisit
 X0= [[0., 0.], [1., 0.,]]
 V0= [[0., 0.], [0., 6.28]]
 M= [1, 3e-6]
+@author: Anni Järvenpää
+"""
+
+import numpy as np
+
+"""
+Lukee tiedostosta alkuarvomatriisit X0, V0 ja M. Ei huomioi #-merkillä alkavia rivejä
 """
 def lueXVM(tiedostonimi):
     f = open(tiedostonimi)
@@ -35,3 +36,44 @@ def lueXVM(tiedostonimi):
                 M.append(float(matriisit[2].strip()))
         rivinro = rivinro + 1
     return [X, V, M]
+    
+"""
+Kirjoittaa matriisin tiedostoon
+@param: kirjoitettava matriisi
+@param: kohdetiedoston nimi
+"""
+def kirjoitaMatr(matriisi, tiedostonimi):
+    tiedosto = open(tiedostonimi, 'w')
+    for i in range(matriisi.shape[0]): # rivi
+        for j in range(len(matriisi[0][0])): # vektori (kappale)
+            for k in range(len(matriisi[0][0][0])): # alkio
+                tiedosto.write(str(matriisi[i][0][j][k]))
+                if (k != len(matriisi[0][0][0])-1):
+                    tiedosto.write(",\t")
+            if (j != len(matriisi[0][0][0])-1):
+                tiedosto.write(";\t")
+        if(i != matriisi.shape[0]-1):
+                tiedosto.write("\n")
+    tiedosto.close()
+
+"""
+Lukee matriiseja sisältävän matriisin tiedostosta
+@param: luettavan tiedoston nimi
+"""
+def lueMatr(tiedostonimi):
+     tiedosto = open(tiedostonimi, 'r')
+     rivit = tiedosto.readlines()
+     palautus = np.ndarray((len(rivit), len(rivit[0].split(";"))), dtype=object)
+     for i in range(len(rivit)):
+        matriisit = [m.strip() for m in rivit[i].strip().split(';')]
+        for j in range(len(matriisit)):
+            try:
+                palautus[i][j] = [float(luku.strip()) for luku in matriisit[j].strip().split(',')]
+            except ValueError:
+                print "Virhe luettaessa alkuarvotiedoston riviä ",i
+                break
+            palautus[i][j] = [float(luku.strip()) for luku in matriisit[j].split(',')]
+                
+     return palautus
+     
+     
